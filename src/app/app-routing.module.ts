85a07b5c9a7ab3dea30823e9fbe047e27e483bcd
Karina <kmirova731@gmail.com>
2023-09-12 22:30:05 +0600

    working version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainPageComponent} from "./main-page/main-page.component";
+import {ProductDetailsComponent} from "./product-details/product-details.component";
 
 const routes: Routes = [
+  {path: 'products/:product_name', component:ProductDetailsComponent},
   {path: '', component: MainPageComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ProductListComponent } from './main-page/product-list/product-list.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { SideMenuComponent } from './main-page/side-menu/side-menu.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     ProductListComponent,
-    TopBarComponent
+    TopBarComponent,
+    SideMenuComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
